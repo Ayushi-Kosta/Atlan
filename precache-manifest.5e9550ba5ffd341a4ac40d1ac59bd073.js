a0ced86eef6b4739d1710eefc26179b97de62945
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-06-07 00:35:06 +0530

    Updates

---
+++ precache-manifest.5e9550ba5ffd341a4ac40d1ac59bd073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742bee65d2df2c4620326395552bbd33",
+    "url": "/Atlan/index.html"
+  },
+  {
+    "revision": "5ce61ff89d92d6b5551e",
+    "url": "/Atlan/static/css/main.4283a763.chunk.css"
+  },
+  {
+    "revision": "70c2746ea73b8dc6f3d3",
+    "url": "/Atlan/static/js/2.8ac14352.chunk.js"
+  },
+  {
+    "revision": "9f6568d034b8dffb4346047f0ba48ca6",
+    "url": "/Atlan/static/js/2.8ac14352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce61ff89d92d6b5551e",
+    "url": "/Atlan/static/js/main.c6d687c0.chunk.js"
+  },
+  {
+    "revision": "557a359ede39207c4a83",
+    "url": "/Atlan/static/js/runtime-main.461e987b.js"
+  }
+]);
